c9df4695027ef0ea33bae1ca4ea0a0b39e57fe37
grachya <grach41@gmail.com>
2020-03-04 13:21:56 +0300

    Address-Book | add address remove handler

--- src/components/AddressTable/AddressTable.js
+++ src/components/AddressTable/AddressTable.js
@@ -6,12 +6,22 @@ class AddressTable extends React.Component {
         super(props)
         this.state = {
             addressList: [
-                {id:1, name: 'Галина', email: 'galina@ya.ru', phone:'89034567582'},
-                {id:2, name: 'Алла', email: 'alla@ya.ru', phone:'89167658734'},
-                {id:3, name: 'Юлия', email: 'yulia@ya.ru', phone:'89264567652'},
+                {id:1, name: 'Галина Петровна', email: 'galina@ya.ru', phone:'89034567582'},
+                {id:2, name: 'Алла Михайловна', email: 'alla@ya.ru', phone:'89167658734'},
+                {id:3, name: 'Юлия Викторовна', email: 'yulia@ya.ru', phone:'89264567652'},
               ]
         }
+        this.removeAdressById = this.removeAdressById.bind(this);
     }
+
+    removeAdressById(addressId) {
+        let addressList = [...this.state.addressList];
+        let index = addressList.find(item => item.id === addressId);
+        if (index) {
+          addressList.splice(index, 1);
+          this.setState({addressList: addressList});
+        }
+      }
 
     render() {
         return (
@@ -25,7 +35,7 @@ class AddressTable extends React.Component {
                                 <td className="table__data table__data--phone">{address.phone}</td>
                                 <td className="table__data table__data--actions">
                                     <button>edit</button>
-                                    <button>remove</button>
+                                    <button onClick={() => this.removeAdressById(address.id)}>remove</button>
                                 </td>
                             </tr>
                         )
