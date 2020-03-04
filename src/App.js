449dec25b4e02e456cfd6a1b8c9eb12185e5b97b
grachya <grach41@gmail.com>
2020-03-04 12:16:49 +0300

    Address-Book | add table component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddressTable from './components/AddressTable/AddressTable';
+
+import './App.scss'
 
 function App() {
   return (
-    <div >
-      
+    <div className="app">
+      <h1>Адресная книга</h1>
+      <AddressTable></AddressTable>
     </div>
   );
 }
---
+++ src/components/AddressTable/AddressTable.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './AddressTable.scss';
+
+class AddressTable extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            addressList: [
+                {id:1, name: 'Галина', email: 'galina@ya.ru', phone:'89034567582'},
+                {id:2, name: 'Алла', email: 'alla@ya.ru', phone:'89167658734'},
+                {id:3, name: 'Юлия', email: 'yulia@ya.ru', phone:'89264567652'},
+              ]
+        }
+    }
+
+    render() {
+        return (
+            <table className="table">
+                <tbody>
+                    {this.state.addressList.map((address, index) => {
+                        return (
+                            <tr key={index} className="table__row">
+                                <td className="table__data table__data--name">{address.name}</td>
+                                <td className="table__data table__data--mail"><a href={"mailto:"+address.email}>{address.email}</a></td>
+                                <td className="table__data table__data--phone">{address.phone}</td>
+                                <td className="table__data table__data--actions">
+                                    <button>edit</button>
+                                    <button>remove</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>    
+            </table>
+        )
+    }
+}
+
+export default AddressTable;
