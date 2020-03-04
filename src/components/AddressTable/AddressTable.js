cce3d683dac34a633375c7c4de4bed7790ee8404
grachya <grach41@gmail.com>
2020-03-04 14:44:37 +0300

    Address-Book | implement adding new address

--- src/components/AddressTable/AddressTable.js
+++ src/components/AddressTable/AddressTable.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './AddressTable.scss';
 
+const initialNewAddressState = {
+    id:"",
+    name: "",
+    email: "",
+    phone: ""
+}
+
 class AddressTable extends React.Component {
     constructor(props){
         super(props)
@@ -9,19 +16,60 @@ class AddressTable extends React.Component {
                 {id:1, name: 'Галина Петровна', email: 'galina@ya.ru', phone:'89034567582'},
                 {id:2, name: 'Алла Михайловна', email: 'alla@ya.ru', phone:'89167658734'},
                 {id:3, name: 'Юлия Викторовна', email: 'yulia@ya.ru', phone:'89264567652'},
-              ]
+            ],
+            newAddress: {
+                id:"",
+                name: "",
+                email: "",
+                phone: ""
+            }
         }
         this.removeAdressById = this.removeAdressById.bind(this);
+        this.addNewAddress = this.addNewAddress.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     removeAdressById(addressId) {
         let addressList = [...this.state.addressList];
-        let index = addressList.find(item => item.id === addressId);
-        if (index) {
-          addressList.splice(index, 1);
-          this.setState({addressList: addressList});
+        let index = addressList.map(item => item.id).indexOf(addressId);
+        addressList.splice(index, 1);
+        this.setState({addressList: addressList});
+    }
+
+    _resetNewAddressState() {
+        this.setState({
+            newAddress: initialNewAddressState
+        });
+    }
+
+    addNewAddress() {
+        if(!this.state.newAddress.name) {
+            alert('Не введено имя адресата!');
+            return;
         }
-      }
+
+        const newAddressRow = {...this.state.newAddress};
+        this.setState((prevState) => ({
+            addressList: [...prevState.addressList, newAddressRow]
+        })); 
+        
+        this._resetNewAddressState();
+    }
+
+    _getNewAddressId() {
+        return this.state.addressList.length ? this.state.addressList[this.state.addressList.length - 1].id + 1 : 1;
+    }
+
+    handleChange(e) {
+        const inputFieldName = e.target.name;
+        const inputFieldValue = e.target.value;
+        const newAddressId = this._getNewAddressId();  
+        const newAddressField = Object.assign({...this.state.newAddress}, {id: newAddressId}, { [inputFieldName]: inputFieldValue })
+        this.setState({
+            newAddress: newAddressField
+        });
+        console.log(this.state.newAddress);
+    }
 
     render() {
         return (
@@ -35,11 +83,44 @@ class AddressTable extends React.Component {
                                 <td className="table__data table__data--phone">{address.phone}</td>
                                 <td className="table__data table__data--actions">
                                     <button>edit</button>
-                                    <button onClick={() => this.removeAdressById(address.id)}>remove</button>
+                                    <button onClick={() => this.removeAdressById(address.id)}>remove {address.id}</button>
                                 </td>
                             </tr>
                         )
                     })}
+                    <tr>
+                        <td className="table__data table__data--edit">
+                            <input 
+                                type="text" 
+                                placeholder="Имя отчество" 
+                                name="name" 
+                                id="name" 
+                                value={this.state.newAddress.name} 
+                                onChange={this.handleChange} />
+                        </td>
+                        <td className="table__data table__data--edit">
+                            <input 
+                                type="email" 
+                                placeholder="Email" 
+                                name="email" 
+                                id="email" 
+                                value={this.state.newAddress.email} 
+                                onChange={this.handleChange} />
+                        </td>
+                        <td className="table__data table__data--edit">
+                            <input 
+                                type="phone" 
+                                placeholder="Номер телфона" 
+                                name="phone" 
+                                id="phone"
+                                value={this.state.newAddress.phone} 
+                                onChange={this.handleChange}  
+                                />
+                        </td>
+                        <td className="table__data table__data--actions">
+                            <button onClick={() => {this.addNewAddress()}}>add</button>
+                        </td>
+                    </tr>
                 </tbody>    
             </table>
         )
