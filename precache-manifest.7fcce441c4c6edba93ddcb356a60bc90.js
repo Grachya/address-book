79e45100e26127c0f210176c1fd7197a7f94312c
grachya <grach41@gmail.com>
2020-03-05 15:59:36 +0300

    Updates

---
+++ precache-manifest.7fcce441c4c6edba93ddcb356a60bc90.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7fc8a7a45ce202e5c258160f200975c",
+    "url": "/address-book-page/index.html"
+  },
+  {
+    "revision": "fdd784250c431c4b9d3b",
+    "url": "/address-book-page/static/css/main.4b4537cf.chunk.css"
+  },
+  {
+    "revision": "fa9e66fb85803757bd1f",
+    "url": "/address-book-page/static/js/2.8ebddf0e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/address-book-page/static/js/2.8ebddf0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd784250c431c4b9d3b",
+    "url": "/address-book-page/static/js/main.764de290.chunk.js"
+  },
+  {
+    "revision": "dfb7ba8a4259f5b2421f",
+    "url": "/address-book-page/static/js/runtime-main.52204bdb.js"
+  },
+  {
+    "revision": "5abde19600441941a1b19b401380f84b",
+    "url": "/address-book-page/static/media/correct.5abde196.svg"
+  },
+  {
+    "revision": "f686e0af39fac85ea1333727932e04a6",
+    "url": "/address-book-page/static/media/cross.f686e0af.svg"
+  },
+  {
+    "revision": "4959a48fbfe1a70679111977f5c4fbf8",
+    "url": "/address-book-page/static/media/edit.4959a48f.svg"
+  },
+  {
+    "revision": "75dc4d4491a4b69e3aa7b92672274291",
+    "url": "/address-book-page/static/media/plus.75dc4d44.svg"
+  }
+]);
